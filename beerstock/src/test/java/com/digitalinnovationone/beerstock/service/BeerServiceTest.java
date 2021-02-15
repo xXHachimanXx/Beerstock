d31c2db33444b2ba225ac80e2dcc3c3172ac4efb
xXHachimanXx <globaldep90@gmail.com>
2021-02-15 11:42:28 -0300

    Creating a method to test the exception thrown when a beer is already registred.

--- beerstock/src/test/java/com/digitalinnovationone/beerstock/service/BeerServiceTest.java
+++ beerstock/src/test/java/com/digitalinnovationone/beerstock/service/BeerServiceTest.java
@@ -6,6 +6,8 @@ import com.digitalinnovationone.beerstock.entity.Beer;
 import com.digitalinnovationone.beerstock.exception.BeerAlreadyRegisteredException;
 import com.digitalinnovationone.beerstock.mapper.BeerMapper;
 import com.digitalinnovationone.beerstock.repository.BeerRepository;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,7 +17,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class BeerServiceTest {
@@ -44,8 +49,25 @@ public class BeerServiceTest {
         //then
         BeerDTO createdBeerDTO = beerService.createBeer(beerDTO);
 
-        assertEquals(beerDTO.getId(), createdBeerDTO.getId());
-        assertEquals(beerDTO.getName(), createdBeerDTO.getName());
+        assertThat(createdBeerDTO.getId(), is(equalTo(beerDTO.getId())));
+        assertThat(createdBeerDTO.getName(), is(equalTo(beerDTO.getName())));
+        assertThat(createdBeerDTO.getQuantity(), is(equalTo(beerDTO.getQuantity())));
+
+    }
+
+    @Test
+    void whenABeerIsAlreadyRegisteredThenThrowAnException() {
+        //given
+        BeerDTO expectedBeerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer duplicatedBeer = beerMapper.toModel(expectedBeerDTO);
+
+        //when
+        // Ensina o Mockito a retornar um Optional vazio quando o findByName e' chamado
+        Mockito.when(beerRepository.findByName(expectedBeerDTO.getName())).thenReturn(Optional.of(duplicatedBeer));
+
+        //then
+        assertThrows(BeerAlreadyRegisteredException.class, () -> beerService.createBeer(expectedBeerDTO));
+
 
     }
 
