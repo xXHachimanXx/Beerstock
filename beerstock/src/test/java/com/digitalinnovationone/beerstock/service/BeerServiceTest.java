62b979f6e6e773ad3c9771c9de596449993a9fb3
xXHachimanXx <globaldep90@gmail.com>
2021-02-16 20:04:08 -0300

    Creating a test to validate that the beer is being returned.

--- beerstock/src/test/java/com/digitalinnovationone/beerstock/service/BeerServiceTest.java
+++ beerstock/src/test/java/com/digitalinnovationone/beerstock/service/BeerServiceTest.java
@@ -4,10 +4,9 @@ import com.digitalinnovationone.beerstock.builder.BeerDTOBuilder;
 import com.digitalinnovationone.beerstock.dto.BeerDTO;
 import com.digitalinnovationone.beerstock.entity.Beer;
 import com.digitalinnovationone.beerstock.exception.BeerAlreadyRegisteredException;
+import com.digitalinnovationone.beerstock.exception.BeerNotFoundException;
 import com.digitalinnovationone.beerstock.mapper.BeerMapper;
 import com.digitalinnovationone.beerstock.repository.BeerRepository;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,10 +16,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BeerServiceTest {
@@ -30,7 +30,7 @@ public class BeerServiceTest {
     @Mock
     private BeerRepository beerRepository;
 
-    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+    private final BeerMapper beerMapper = BeerMapper.INSTANCE;
 
     @InjectMocks
     private BeerService beerService;
@@ -43,8 +43,8 @@ public class BeerServiceTest {
 
         //when
         // Ensina o Mockito a retornar um Optional vazio quando o findByName e' chamado
-        Mockito.when(beerRepository.findByName(beerDTO.getName())).thenReturn(Optional.empty());
-        Mockito.when(beerRepository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
+        when(beerRepository.findByName(beerDTO.getName())).thenReturn(Optional.empty());
+        when(beerRepository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
 
         //then
         BeerDTO createdBeerDTO = beerService.createBeer(beerDTO);
@@ -63,13 +63,26 @@ public class BeerServiceTest {
 
         //when
         // Ensina o Mockito a retornar um Optional vazio quando o findByName e' chamado
-        Mockito.when(beerRepository.findByName(expectedBeerDTO.getName())).thenReturn(Optional.of(duplicatedBeer));
+        when(beerRepository.findByName(expectedBeerDTO.getName())).thenReturn(Optional.of(duplicatedBeer));
 
         //then
         assertThrows(BeerAlreadyRegisteredException.class, () -> beerService.createBeer(expectedBeerDTO));
 
-
     }
 
+    @Test
+    void whenAValidBeerNameIsGivenThenReturnABeer() throws BeerNotFoundException {
+        // given
+        BeerDTO expectedFoundBeerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer expectedFoundBeer = beerMapper.toModel(expectedFoundBeerDTO);
+
+        // when
+        when(beerRepository.findByName(expectedFoundBeer.getName())).thenReturn(Optional.of(expectedFoundBeer));
+
+        // then
+        BeerDTO foundBeerDTO = beerService.findByName(expectedFoundBeerDTO.getName());
+        assertThat(foundBeerDTO, is(equalTo(expectedFoundBeerDTO)));
+
+    }
 
 }
